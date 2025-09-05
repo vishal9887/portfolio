beadd21750ff9f569709c6932a7fe826ee152b14
vishal9887 <149265626+vishal9887@users.noreply.github.com>
2025-09-05 09:58:09 +0530

    Update Resume.jsx

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,36 +1,38 @@
-import React from 'react';
-import { backend } from '../assets';
-import { Tilt } from 'react-tilt';
-import { motion } from 'framer-motion';
-import { fadeIn } from '../utils/motion';
-import { SectionWrapper } from '../hoc';
+import React from "react";
+import { backend } from "../assets";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { fadeIn } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
 
 const Resume = () => {
   return (
-    <div className='w-full mt-5 max-h-screen relative'>
-      <div className='text-[15px] font-semibold text-white-100'>MY</div>
-      <div className='text-[60px] font-extrabold'>Resume.</div>
+    <div className="w-full mt-5 max-h-screen relative">
+      <div className="text-[15px] font-semibold text-white">MY</div>
+      <div className="text-[60px] font-extrabold">Resume.</div>
 
-      <Tilt className="xs:w-[250px] mt-11 min-w-full mx-auto">
+      <Tilt
+        className="xs:w-[250px] mt-11 min-w-full mx-auto"
+        options={{ max: 45, scale: 1, speed: 450 }}
+      >
         <motion.div
           variants={fadeIn("right", "spring", 0.5 * 1, 0.75)}
-          className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+          className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
         >
-          <div
-            options={{
-              max: 45,
-              scale: 1,
-              speed: 450
-            }}
-            className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex flex-col justify-evenly items-center'
-          >
-            <img src={backend} alt="resume" className='w-16 h-16 object-contain' />
-            <h3
-  className='text-white text-[20px] font-bold text-center cursor-pointer'
-  onClick={() => window.open("https://drive.google.com/file/d/1LW2f1stUYb4yu8LSbcy_mFyMyRTGuZdC/view?usp=sharing")}
->
-  Click Here
-</h3>
+          <div className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex flex-col justify-evenly items-center">
+            <img
+              src={backend}
+              alt="resume"
+              className="w-16 h-16 object-contain"
+            />
+            <a
+              href="https://drive.google.com/file/d/1LW2f1stUYb4yu8LSbcy_mFyMyRTGuZdC/view?usp=sharing"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white text-[20px] font-bold text-center cursor-pointer hover:underline"
+            >
+              Click Here
+            </a>
           </div>
         </motion.div>
       </Tilt>
